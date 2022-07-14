d14dc3baac7b0bbbe6733aa35f71276ab695dc3c
Stepan Kosatyi <stepan@kosatyi.com>
2022-07-14 19:41:38 +0300

    refactor

--- rollup.config.js
+++ rollup.config.js
@@ -2,58 +2,55 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import ignore from 'rollup-plugin-ignore'
+import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
+
 const external = ['path', 'fs', 'chokidar']
+
+const babelConfig = {
+    babelHelpers: 'bundled',
+    presets: ['@babel/preset-env'],
+}
 
 const iife = {
     input: 'src/index.js',
-    output: {
-        name: 'ejs',
-        file: pkg.browser,
-        format: 'umd',
-    },
-    plugins: [
-        ignore(external),
-        resolve({
-            browser: true,
-            preferBuiltins: false,
-        }),
-        commonjs({}),
-        babel({
-            babelHelpers: 'bundled',
-            presets: ['@babel/preset-env'],
-        }),
+    output: [
+        {
+            name: 'ejs',
+            file: pkg.browser,
+            format: 'umd',
+        },
+        {
+            name: 'ejs',
+            file: pkg.minified,
+            format: 'umd',
+            plugins: [terser()],
+        },
     ],
+    plugins: [ignore(external), resolve({}), commonjs({}), babel(babelConfig)],
 }
 
 const cjs = {
     input: 'src/index.js',
+    external: external,
     output: {
         name: 'ejs',
         file: pkg.main,
         format: 'cjs',
         exports: 'default',
     },
-    external: ['fs', 'path', 'chokidar'],
-    plugins: [
-        resolve({}),
-        commonjs({}),
-        babel({
-            babelHelpers: 'bundled',
-            presets: ['@babel/preset-env'],
-        }),
-    ],
+    plugins: [resolve({}), commonjs({}), babel(babelConfig)],
 }
 
 const mjs = {
     input: 'src/index.js',
+    external: external,
     output: {
         name: 'ejs',
         file: pkg.module,
         format: 'esm',
     },
-    external: ['fs', 'path', 'chokidar'],
     plugins: [resolve({}), commonjs({})],
 }
 
